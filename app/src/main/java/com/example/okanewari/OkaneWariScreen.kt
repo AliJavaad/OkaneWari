a2affcf1271086e55daa8312795062003ece33e9
Javaad <javs_ali@outlook.com>
2025-03-07 11:39:42 +0900

    Added top app bar

--- app/src/main/java/com/example/okanewari/OkaneWariScreen.kt
+++ app/src/main/java/com/example/okanewari/OkaneWariScreen.kt
@@ -2,12 +2,25 @@ package com.example.okanewari
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.okanewari.ui.AddExpenseScreen
 import com.example.okanewari.ui.AddPartyScreen
@@ -15,19 +28,66 @@ import com.example.okanewari.ui.ListPartysScreen
 import com.example.okanewari.ui.PartyScreen
 
 // Enum defines the ROUTES for navigation
-enum class OkaneWariScreen(){
-    ListPartys,
-    ShowParty,
-    AddParty,
-    AddExpense
+enum class OkaneWariScreen(val title: Int){
+    ListPartys(title = R.string.home_screen),
+    ShowParty(title = R.string.my_party),
+    AddParty(title = R.string.add_party),
+    AddExpense(title = R.string.add_expense)
 }
+
+/**
+ * Composable that displays the topBar and displays back button if back navigation is possible.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OkaneWariAppBar(
+    currentScreen: OkaneWariScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        // TODO change the title depending on the party name
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
 
 @Composable
 fun OkaneWariApp(
     navController: NavHostController = rememberNavController()
 ){
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = OkaneWariScreen.valueOf(
+        backStackEntry?.destination?.route ?: OkaneWariScreen.ListPartys.name
+    )
     // The view
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        // Creating the top app bar
+        topBar = {
+            OkaneWariAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
         NavHost(
             // Initialize the navController to the party list view
             navController = navController,
---
+++ app/src/main/java/com/example/okanewari/ui/components/DoneAndCancelButtons.kt
@@ -0,0 +1,52 @@
+package com.example.okanewari.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.example.okanewari.R
+
+@Composable
+fun DoneAndCancelButtons(
+    doneButtonClick: () -> Unit,
+    cancelButtonClick: () -> Unit
+) {
+    val mediumPadding = dimensionResource(R.dimen.medium_padding)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.spacedBy(mediumPadding),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = doneButtonClick
+        ) {
+            Text(
+                text = stringResource(R.string.done),
+                fontSize = 16.sp
+            )
+        }
+
+        OutlinedButton(
+            onClick = cancelButtonClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.cancel),
+                fontSize = 16.sp
+            )
+        }
+    }
+}
