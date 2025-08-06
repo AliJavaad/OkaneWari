ee33013c1d5f76b2ae853af971f5610eb5bde2e0
Javaad <javs_ali@outlook.com>
2025-08-06 10:30:45 +0900

    AddExpense backend test for calculating different split types

--- app/src/androidTest/kotlin/AddExpenseTesting.kt
+++ app/src/androidTest/kotlin/AddExpenseTesting.kt
@@ -9,6 +9,7 @@ import com.example.okanewari.data.OfflineOkaneWariRepository
 import com.example.okanewari.data.OkaneWariDatabase
 import com.example.okanewari.data.OkaneWariRepository
 import com.example.okanewari.data.PartyModel
+import com.example.okanewari.data.SplitModel
 import com.example.okanewari.data.SplitType
 import com.example.okanewari.ui.components.toExpenseDetails
 import com.example.okanewari.ui.components.toExpenseUiState
@@ -38,10 +39,19 @@ class AddExpenseTesting {
     private val member2 = MemberModel(2, 1, "hal")
     private val member3 = MemberModel(3, 1, "ivan")
     private val member4 = MemberModel(4, 1, "kent")
-    private val expense1 = ExpenseModel(1, 1, "food","400", Date().time)
+    private val expense1 = ExpenseModel(1, 1, "food","300", Date().time)
 
-//    @get:Rule
-//    val composeTestRule = createComposeRule()
+    // Holders to check the split data
+    private val splitListPas = listOf(
+        SplitModel(1, 1,1,1, SplitType.PAID_AND_SPLIT, "200.00"),
+        SplitModel(2, 1,1,2, SplitType.OWE, "-100.00"),
+        SplitModel(3, 1,1,3, SplitType.OWE, "-100.00")
+    )
+    private val splitListPif = listOf(
+        SplitModel(1, 1,1,1, SplitType.PAID_IN_FULL, "300"),
+        SplitModel(2, 1,1,2, SplitType.OWE, "-150.00"),
+        SplitModel(3, 1,1,3, SplitType.OWE, "-150.00")
+    )
 
     @Before
     fun createDb() {
@@ -68,7 +78,7 @@ class AddExpenseTesting {
 
     @Test
     @Throws(Exception::class)
-    fun addExpenseAndCheckSplit() = runBlocking{
+    fun insertBasicExpenseWithPaidAndSplit_CheckExpenseAndSplits() = runBlocking{
         // Prepopulate the database
         populatePartyAndMemberTables()
         // init the viewmodel
@@ -95,6 +105,45 @@ class AddExpenseTesting {
         // retrieve data from database and confirm the splits
         val expense = owRepository.getAllExpensesStream(1).first()
         assertEquals(expense1, expense[0])
+
+        // Check the splits
+        val splits = owRepository.getAllSplitsForExpense(1).first()
+        assertEquals(splitListPas, splits)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertBasicExpenseWithPaidInFull_CheckExpenseAndSplits() = runBlocking{
+        // Prepopulate the database
+        populatePartyAndMemberTables()
+        // init the viewmodel
+        val testPartyId = 1L
+        val savedStateHandle = SavedStateHandle().apply {
+            set(ListExpensesDestination.partyIdArg, testPartyId)
+        }
+        val viewModel = AddExpenseViewModel(savedStateHandle, owRepository)
+
+        // add test data to the viewmodel
+        viewModel.updateExpenseUiState(
+            expenseDetails = expense1.toExpenseDetails(),
+            partyDetails = party1.toPartyDetails()
+        )
+        viewModel.updateSplitUiState(
+            payingMember = member1.toMemberDetails(),
+            owingMembers = listOf(member2, member3),
+            payType = SplitType.PAID_IN_FULL
+        )
+
+        // run the backend to add expense
+        viewModel.saveExpenseAndSplit()
+
+        // retrieve data from database and confirm the splits
+        val expense = owRepository.getAllExpensesStream(1).first()
+        assertEquals(expense1, expense[0])
+
+        // Check the splits
+        val splits = owRepository.getAllSplitsForExpense(1).first()
+        assertEquals(splitListPif, splits)
     }
 
     private suspend fun populatePartyAndMemberTables(){
