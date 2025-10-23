14112aa82bcab2069f3c815801548a9f275da682
Javs <javs_ali@outlook.com>
2025-10-23 10:33:36 +0900

    Targetting API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.javs.okanewari"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.javs.okanewari"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
